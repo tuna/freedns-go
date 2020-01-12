dc45e02d25dceaae42e93d2d87caaf0c1ef48eb1
louchenyao <louchenyao@gmail.com>
2020-01-12 22:01:10 +0800

    add tests for spoofing_proof_resolver

---
+++ freedns/resolve_test.go
@@ -0,0 +1,64 @@
+package freedns
+
+import (
+	"testing"
+	"time"
+
+	goc "github.com/louchenyao/golang-cache"
+	"github.com/miekg/dns"
+)
+
+func Test_spoofing_proof_resolver_resolve(t *testing.T) {
+	c, _ := goc.NewCache("lru", 1024)
+	resolver := &spoofing_proof_resolver{
+		fast_upstream:  "114.114.114.114:53",
+		clean_upstream: "8.8.8.8:53",
+		cn_domains:     c,
+	}
+
+	tests := []struct {
+		domain            string
+		qtype             uint16
+		net               string
+		expected_upstream string
+	}{
+		// expect 8.8.8.8 as the upstream b/c the resolver have
+		// no way to identify this is an China domain without A records
+		{"ustc.edu.cn.", dns.TypeMX, "udp", "8.8.8.8:53"},
+		{"ustc.edu.cn.", dns.TypeA, "udp", "114.114.114.114:53"},
+		// after querying the A record of ustc.edu.cn,
+		// the resolver should know this is an China domain
+		{"ustc.edu.cn.", dns.TypeMX, "udp", "114.114.114.114:53"},
+		{"google.com.", dns.TypeA, "udp", "8.8.8.8:53"},
+		{"mi.cn.", dns.TypeA, "udp", "114.114.114.114:53"},
+		{"xiaomi.com.", dns.TypeA, "udp", "114.114.114.114:53"},
+		{"youtube.com.", dns.TypeA, "udp", "8.8.8.8:53"},
+		{"www.tsinghua.edu.cn.", dns.TypeA, "tcp", "114.114.114.114:53"},
+		{"twitter.com.", dns.TypeA, "tcp", "8.8.8.8:53"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.domain, func(t *testing.T) {
+			req := &dns.Msg{
+				MsgHdr: dns.MsgHdr{
+					RecursionDesired: true,
+				},
+				Question: []dns.Question{dns.Question{
+					Name:   tt.domain,
+					Qtype:  tt.qtype,
+					Qclass: dns.ClassINET,
+				}},
+			}
+			start := time.Now()
+			res, upstream := resolver.resolve(req, tt.net)
+			end := time.Now()
+			elapsed := end.Sub(start)
+			if upstream != tt.expected_upstream {
+				t.Errorf("spoofing_proof_resolver.resolve() got1 = %v, want %v", upstream, tt.expected_upstream)
+			}
+			if len(res.Answer) == 0 {
+				t.Errorf("Expect returning at least one answer")
+			}
+			t.Logf("spoofing_proof_resolver.resolve() domain = %v, net = %v, elapsed = %v, record = %v", tt.domain, tt.net, elapsed, res)
+		})
+	}
+}
